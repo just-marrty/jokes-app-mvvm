dc38f4f8903780f3a2dd64237623bac63f64c713
Martin <marrty.h.1992@gmail.com>
2026-01-07 18:54:37 +0100

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Martin Hrbáček on 07.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
